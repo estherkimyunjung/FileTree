9e5c0b13d77a1d22bb75b4160694fec1575c37ff
estherkimyunjung <estherkimyunjung@gmail.com>
2021-02-08 23:43:10 -0600

    setState by react hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <Folder name="Desktop" isOpen={false}>
+      <Folder name="Music">
+        <File name="all_star.mp4"/>
+        <File name="express_file.mp4"/>
+      </Folder>
+    <File name="dog.jpeg"/>
+    <File name="cat.png"/>
+    </Folder>
+    <Folder name="Application"/>
+
+  </div>
   );
 }
 
+
+const Folder = (props) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const {name, children} = props
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return <div>
+      <span onClick={handleClick}>{name}</span>
+      <div style={{marginLeft: '17px'}}>
+        {isOpen ? children : null}
+      </div>
+    </div>
+};
+const File = (props) => {
+  return <div>{props.name}</div>
+};
+
 export default App;
+
